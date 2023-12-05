c8726a656b85657aff3c4dd37449ac7b2b641aa4
Dan23333333 <gorbikdan8@gmail.com>
2023-12-05 16:27:45 +0200

    modified:   Booking/model.py

--- Booking/model.py
+++ Booking/model.py
@@ -1,34 +1,83 @@
+
+
+from sqlalchemy import Column, Integer, Date, Numeric
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# З'єднання з базою даних
+DATABASE_URL = 'postgresql://postgres:1@localhost:5432/postgres'  # Приклад URL для PostgreSQL
+engine = create_engine(DATABASE_URL)
+
+# Створення базового класу, від якого будуть наслідуватися всі моделі
+Base = declarative_base()
+
+# Створення сесії для взаємодії з базою даних
+Session = sessionmaker()
+session = Session()
+
+class BookingTicket(Base):
+    __tablename__ = 'booking_ticket'
+    #__table_args__ = {'bind': engine}
+    booking_id = Column(Integer, primary_key=True)
+    client_id = Column(Integer, nullable=False)
+    room_number = Column(Integer, nullable=False)
+    booking_start_date = Column(Date, nullable=False)
+    booking_end_date = Column(Date, nullable=False)
+    price = Column(Numeric(10, 2), nullable=False)
+
 # ModelBookingTicket
 #################################################################################
 class ModelBookingTicket:
     def __init__(self, db_model):
         self.conn = db_model.conn
+        #self.session = db_model.session
+        self.engine = create_engine(DATABASE_URL)
+        self.session = Session()
+    # def add_booking_ticket(self, booking_id, client_id, room_number, booking_start_date, booking_end_date, price):
+    #     c = self.conn.cursor()
+    #     try:
+    #         # Check if client_id and room_number match parent tables
+    #         c.execute('SELECT 1 FROM client WHERE client_id = %s', (client_id,))
+    #         client_exists = c.fetchone()
+    #
+    #         c.execute('SELECT 1 FROM room WHERE room_number = %s', (room_number,))
+    #         room_exists = c.fetchone()
+    #
+    #         if not client_exists or not room_exists:
+    #             # Return an exception notification and throw an error
+    #             return False  # Or throw an exception to process it further
+    #         else:
+    #             # All checks have passed, insert into booking_ticket
+    #             c.execute(
+    #                 'INSERT INTO booking_ticket (booking_id, client_id, room_number, '
+    #                 'booking_start_date, booking_end_date, price) VALUES (%s, %s, %s, %s, %s, %s)',
+    #                 (booking_id, client_id, room_number, booking_start_date, booking_end_date, price))
+    #             self.conn.commit()
+    #             return True
+    #     except Exception as e:
+    #         self.conn.rollback()
+    #         print(f"Error when adding a booking: {str(e)}")
+    #         return False
 
     def add_booking_ticket(self, booking_id, client_id, room_number, booking_start_date, booking_end_date, price):
-        c = self.conn.cursor()
         try:
-            # Check if client_id and room_number match parent tables
-            c.execute('SELECT 1 FROM client WHERE client_id = %s', (client_id,))
-            client_exists = c.fetchone()
-
-            c.execute('SELECT 1 FROM room WHERE room_number = %s', (room_number,))
-            room_exists = c.fetchone()
-
-            if not client_exists or not room_exists:
-                # Return an exception notification and throw an error
-                return False  # Or throw an exception to process it further
-            else:
-                # All checks have passed, insert into booking_ticket
-                c.execute(
-                    'INSERT INTO booking_ticket (booking_id, client_id, room_number, '
-                    'booking_start_date, booking_end_date, price) VALUES (%s, %s, %s, %s, %s, %s)',
-                    (booking_id, client_id, room_number, booking_start_date, booking_end_date, price))
-                self.conn.commit()
-                return True
+            new_booking = BookingTicket(
+                booking_id=booking_id,
+                client_id=client_id,
+                room_number=room_number,
+                booking_start_date=booking_start_date,
+                booking_end_date=booking_end_date,
+                price=price
+            )
+            self.session.add(new_booking)
+            self.session.commit()
+            return True
         except Exception as e:
-            self.conn.rollback()
+            self.session.rollback()
             print(f"Error when adding a booking: {str(e)}")
             return False
+
 
     def get_all_booking_tickets(self):
         c = self.conn.cursor()
@@ -36,32 +85,74 @@ class ModelBookingTicket:
         return c.fetchall()
 
     def update_booking_ticket(self, booking_id, client_id, room_number, booking_start_date, booking_end_date, price):
-        c = self.conn.cursor()
         try:
-            # Attempting to update a record
-            c.execute('UPDATE booking_ticket SET client_id=%s, room_number=%s, booking_start_date=%s, '
-                      'booking_end_date=%s, price=%s WHERE booking_id=%s',
-                      (client_id, room_number, booking_start_date, booking_end_date, price, booking_id))
-            self.conn.commit()
-            return True  # Returns True if the update was successful
+            # Отримуємо бронювання з бази даних за його унікальним ідентифікатором
+            booking = self.session.query(BookingTicket).filter_by(booking_id=booking_id).first()
+
+            # Перевіряємо, чи бронювання існує у базі даних
+            if booking:
+                # Оновлюємо дані про бронювання
+                booking.client_id = client_id
+                booking.room_number = room_number
+                booking.booking_start_date = booking_start_date
+                booking.booking_end_date = booking_end_date
+                booking.price = price
+
+                self.session.commit()
+                return True  # Повертає True у випадку успішного оновлення
+            else:
+                return False  # Повертає False, якщо бронювання не знайдено
         except Exception as e:
-            # Handling an error if the update failed
-            self.conn.rollback()
+            self.session.rollback()
             print(f"Error when updating a reservation: {str(e)}")
-            return False   # Returns False if insertion fails
+            return False  # Повертає False у випадку помилки під час оновлення
+
+
+    # def update_booking_ticket(self, booking_id, client_id, room_number, booking_start_date, booking_end_date, price):
+    #     c = self.conn.cursor()
+    #     try:
+    #         # Attempting to update a record
+    #         c.execute('UPDATE booking_ticket SET client_id=%s, room_number=%s, booking_start_date=%s, '
+    #                   'booking_end_date=%s, price=%s WHERE booking_id=%s',
+    #                   (client_id, room_number, booking_start_date, booking_end_date, price, booking_id))
+    #         self.conn.commit()
+    #         return True  # Returns True if the update was successful
+    #     except Exception as e:
+    #         # Handling an error if the update failed
+    #         self.conn.rollback()
+    #         print(f"Error when updating a reservation: {str(e)}")
+    #         return False   # Returns False if insertion fails
 
     def delete_booking_ticket(self, booking_id):
-        c = self.conn.cursor()
         try:
-            # Attempting to update a record
-            c.execute('DELETE FROM booking_ticket WHERE booking_id=%s', (booking_id,))
-            self.conn.commit()
-            return True  # Returns True if the update was successful
+            # Отримуємо бронювання з бази даних за його унікальним ідентифікатором
+            booking = self.session.query(BookingTicket).filter_by(booking_id=booking_id).first()
+
+            # Перевіряємо, чи бронювання існує у базі даних
+            if booking:
+                # Видаляємо бронювання з сесії SQLAlchemy
+                self.session.delete(booking)
+                self.session.commit()
+                return True  # Повертає True у випадку успішного видалення
+            else:
+                return False  # Повертає False, якщо бронювання не знайдено
         except Exception as e:
-            # Handling an error in case the deletion failed
-            self.conn.rollback()
+            self.session.rollback()
             print(f"Error when deleting a reservation: {str(e)}")
-            return False   # Returns False if insertion fails
+            return False  # Повертає False у випадку помилки під час видалення
+
+    # def delete_booking_ticket(self, booking_id):
+    #     c = self.conn.cursor()
+    #     try:
+    #         # Attempting to update a record
+    #         c.execute('DELETE FROM booking_ticket WHERE booking_id=%s', (booking_id,))
+    #         self.conn.commit()
+    #         return True  # Returns True if the update was successful
+    #     except Exception as e:
+    #         # Handling an error in case the deletion failed
+    #         self.conn.rollback()
+    #         print(f"Error when deleting a reservation: {str(e)}")
+    #         return False   # Returns False if insertion fails
 
     def check_booking_existence(self, booking_id):
         c = self.conn.cursor()
@@ -119,4 +210,3 @@ class ModelBookingTicket:
             self.conn.rollback()
             print(f"Error when adding a client: {str(e)}")
             return False  # Returns False if insertion fails
-
--- model.py
+++ model.py
@@ -1,39 +1,57 @@
-from sqlalchemy import create_engine, Column, Integer, String, Date, DECIMAL
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+import psycopg2
 
-Base = declarative_base()
-
-class BookingTicket(Base):
-    __tablename__ = 'booking_ticket'
-
-    booking_id = Column(Integer, primary_key=True)
-    client_id = Column(Integer, nullable=False)
-    room_number = Column(Integer, nullable=False)
-    booking_start_date = Column(Date, nullable=False)
-    booking_end_date = Column(Date, nullable=False)
-    price = Column(DECIMAL(10, 2), nullable=False)
-
-class Client(Base):
-    __tablename__ = 'client'
-
-    client_id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    surname = Column(String, nullable=False)
-    email = Column(String)
-
-class Room(Base):
-    __tablename__ = 'room'
-
-    room_number = Column(Integer, primary_key=True)
-    room_type = Column(String, nullable=False)
 
 class Model:
     def __init__(self):
-        self.engine = create_engine('postgresql://postgres:1@localhost/postgres')
-        Base.metadata.create_all(self.engine)
-        Session = sessionmaker(bind=self.engine)
-        self.session = Session()
+        self.conn = psycopg2.connect(
+            dbname='postgres',
+            user='postgres',
+            password='1',
+            host='localhost',
+            port=5432
+        )
+        self.create_tables()
 
-    def close_connection(self):
-        self.session.close()
+    def create_tables(self):
+        c = self.conn.cursor()
+        # Check for tables
+        c.execute("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'booking_ticket')")
+        booking_ticket_table_exists = c.fetchone()[0]
+
+        c.execute("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'client')")
+        client_table_exists = c.fetchone()[0]
+
+        c.execute("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = 'room')")
+        room_table_exists = c.fetchone()[0]
+
+        if not booking_ticket_table_exists:
+            c.execute('''
+                        CREATE TABLE booking_ticket (
+                            booking_id SERIAL PRIMARY KEY,
+                            client_id INTEGER NOT NULL,
+                            room_number INTEGER NOT NULL,
+                            booking_start_date DATE NOT NULL,
+                            booking_end_date DATE NOT NULL,
+                            price DECIMAL(10, 2) NOT NULL
+                        )
+                    ''')
+        if not client_table_exists:
+            c.execute('''
+                        CREATE TABLE client (
+                            client_id SERIAL PRIMARY KEY,
+                            name TEXT NOT NULL,
+                            surname TEXT NOT NULL,
+                            email TEXT
+                        )
+                    ''')
+        if not room_table_exists:
+            c.execute('''
+                        CREATE TABLE room (
+                            room_number SERIAL PRIMARY KEY,
+                            room_type TEXT NOT NULL
+                        )
+                    ''')
+
+        self.conn.commit()
+
+
